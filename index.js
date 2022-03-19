f3caea25087f82ab5ca2606b2088b51eb13c7ccf
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-03-19 13:56:57 -0400

    transfer to node

---
+++ index.js
@@ -0,0 +1,35 @@
+const exp = require('express');
+const app = exp();
+const bp = require('body-parser');
+const https = require('https');
+app.use(bp.urlencoded({extended: true})); // necessary to parse html 
+const request = require('request');
+const res = require('express/lib/response');
+app.use(exp.static('public'));//loads html supporting files when sending responses to browser
+
+app.use(bp.urlencoded({extended: true}));  
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/home.html');
+})
+
+app.get('/blog', function(req, res){
+    
+    res.sendFile(__dirname + '/blog.html');
+})
+
+app.get('/user', function(req, res){
+    res.sendFile(__dirname + '/users.html');
+})
+
+app.get('/addPost', function(req, res){
+    res.sendFile(__dirname + '/addPost.html');
+})
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('server is live!')
+})
+
+app.get('/user', function(req, res){
+    res.sendFile(__dirname + '/users.html');
+})
