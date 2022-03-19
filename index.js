19d8fe0f1fb17e2dfff3721a94763a372d730205
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-03-19 16:47:38 -0400

    add routes and blog post page

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const https = require('https');
 app.use(bp.urlencoded({extended: true})); // necessary to parse html 
 const request = require('request');
 const res = require('express/lib/response');
+const fs = require('fs');
+const { title } = require('process');
+const HTMLParser = require('node-html-parser');
+const blog = HTMLParser.parse(__dirname + '/blog.html')
+
+
 app.use(exp.static('public'));//loads html supporting files when sending responses to browser
 
 app.use(bp.urlencoded({extended: true}));  
@@ -14,6 +20,14 @@ app.get('/', function(req, res){
 })
 
 app.get('/blog', function(req, res){
+
+    fs.readFile(__dirname +'/database.json', 'utf8', (err, jsonString) => {
+            const database = JSON.parse(jsonString)
+            console.log(database.title1[0]);
+            const blogTitle = database.title1;
+    })
+
+    console.log(blog);
     
     res.sendFile(__dirname + '/blog.html');
 })
@@ -26,18 +40,32 @@ app.get('/addPost', function(req, res){
     res.sendFile(__dirname + '/addPost.html');
 })
 
-app.listen(process.env.PORT || 3000, () =>{
-    console.log('server is live!')
-})
-
 app.post('/addPost', function(req, res){
     console.log(req.body.title)
     console.log(req.body.description)
     console.log(req.body.posterName)
     console.log(req.body.posting)
+
+    fs.readFile(__dirname +'/database.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err)
+            return
+        }
+        try {
+            const database = JSON.parse(jsonString)
+            console.log(database.title1)
+    } catch(err) {
+            console.log('Error parsing JSON string:', err)
+        }
+    })
+
     res.redirect('/addPost')
 })
 
-app.get('/user', function(req, res){
-    res.sendFile(__dirname + '/users.html');
+app.get('/blogPost', function(req, res){
+    res.sendFile(__dirname + '/blogPost.html');
+})
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('server is live!')
 })
