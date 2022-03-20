a3ce33b15f6e0bca13feee22ddcef6d625479a65
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-03-19 21:59:22 -0400

    add gitignore and update routes

--- index.js
+++ index.js
@@ -2,42 +2,24 @@ const exp = require('express');
 const app = exp();
 const bp = require('body-parser');
 const https = require('https');
-app.use(bp.urlencoded({extended: true})); // necessary to parse html 
-const request = require('request');
-const res = require('express/lib/response');
-const fs = require('fs');
-const { title } = require('process');
-const HTMLParser = require('node-html-parser');
-const blog = HTMLParser.parse(__dirname + '/blog.html')
-
-
-app.use(exp.static('public'));//loads html supporting files when sending responses to browser
-
+app.use(bp.urlencoded({extended: true}));
+app.use(exp.static('public'));
 app.use(bp.urlencoded({extended: true}));  
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/home.html');
+    res.sendFile(__dirname + '/views/home.html');
 })
 
 app.get('/blog', function(req, res){
-
-    fs.readFile(__dirname +'/database.json', 'utf8', (err, jsonString) => {
-            const database = JSON.parse(jsonString)
-            //console.log(database.title1[0]);
-            const blogTitle = database.title1;
-    })
-
-    //console.log(blog);
-    
-    res.sendFile(__dirname + '/blog.html');
+    res.sendFile(__dirname + '/views/blog.html');
 })
 
 app.get('/user', function(req, res){
-    res.sendFile(__dirname + '/users.html');
+    res.sendFile(__dirname + '/views/users.html');
 })
 
 app.get('/addPost', function(req, res){
-    res.sendFile(__dirname + '/addPost.html');
+    res.sendFile(__dirname + '/views/addPost.html');
 })
 
 app.post('/addPost', function(req, res){
@@ -45,37 +27,23 @@ app.post('/addPost', function(req, res){
     console.log(req.body.description)
     console.log(req.body.posterName)
     console.log(req.body.posting)
-
-    fs.readFile(__dirname +'/database.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err)
-            return
-        }
-        try {
-            const database = JSON.parse(jsonString)
-            //console.log(database.title1)
-    } catch(err) {
-            console.log('Error parsing JSON string:', err)
-        }
-    })
-
-    res.redirect('/addPost')
+    res.sendFile(__dirname + '/views/success.html');
 })
 
 app.get('/blogPost', function(req, res){
-    res.sendFile(__dirname + '/blogPost.html');
+    res.sendFile(__dirname + '/views/blogPost.html');
 })
 app.get('/blogPost2', function(req, res){
-    res.sendFile(__dirname + '/blogPost2.html');
+    res.sendFile(__dirname + '/views/blogPost2.html');
 })
 app.get('/blogPost3', function(req, res){
-    res.sendFile(__dirname + '/blogPost3.html');
+    res.sendFile(__dirname + '/views/blogPost3.html');
 })
 app.get('/blogPost4', function(req, res){
-    res.sendFile(__dirname + '/blogPost4.html');
+    res.sendFile(__dirname + '/views/blogPost4.html');
 })
 app.get('/blogPost5', function(req, res){
-    res.sendFile(__dirname + '/blogPost5.html');
+    res.sendFile(__dirname + '/views/blogPost5.html');
 })
 
 
