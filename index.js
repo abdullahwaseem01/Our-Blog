e48946d3d15f58fdfdeb55d3feb145633b278408
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-03-23 17:19:17 -0400

    stable pull from firebase and post to blog page

--- index.js
+++ index.js
@@ -4,50 +4,52 @@ const bp = require('body-parser');
 const https = require('https');
 const ejs = require('ejs');
 
-app.use(bp.urlencoded({extended: true}));
+app.use(bp.urlencoded({ extended: true }));
 app.use(exp.static('public'));
 app.set('view engine', 'ejs');
 
 
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
-admin.initializeApp({credential: admin.credential.cert(serviceAccount)});  
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
 
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('home');
 })
 
 let blogsRef = db.collection('Blogs');
 
-app.get('/blog', function(req, res){
+app.get('/blog', function (req, res) {
     var posts = [];
     var eachPost = [];
-    blogsRef.get().then((qureySnapshot) =>{
-        qureySnapshot.forEach(document =>{
+    blogsRef.get().then((qureySnapshot) => {
+        qureySnapshot.forEach(document => {
             // console.log(document.data().title);
             // console.log(document.data().description);
             // console.log(document.data().name);
             // console.log(document.data().posting);
-            eachPost.push(document.data().title, document.data().description, document.data().posting )
+            eachPost.push(document.data().title);
+            eachPost.push(document.data().description);
+            eachPost.push(document.data().name); 
             posts.push(eachPost);
         })
-
-        // console.log('successfully read all documents');
+        res.render('blog', {
+            posts: posts
+        });
     })
-    
-    res.render('blog');
+
 })
 
-app.get('/blogpost', (req, res)=>{
+app.get('/blogpost', (req, res) => {
     res.redirect('blog')
 })
 
-app.get('/user', function(req, res){
-    blogsRef.get().then((qureySnapshot) =>{
-        qureySnapshot.forEach(document =>{
+app.get('/user', function (req, res) {
+    blogsRef.get().then((qureySnapshot) => {
+        qureySnapshot.forEach(document => {
             // console.log(document.data().name);
         })
         // console.log('successfully author names');
@@ -56,16 +58,16 @@ app.get('/user', function(req, res){
     res.render('users');
 })
 
-app.get('/addPost', function(req, res){
+app.get('/addPost', function (req, res) {
     res.render('addPost');
 })
 
-app.post('/addPost', function(req, res){
+app.post('/addPost', function (req, res) {
     const submission = {
         title: req.body.title,
         description: req.body.description,
         name: req.body.posterName,
-        posting: req.body.posting 
+        posting: req.body.posting
     };
     // db.collection('Blogs').doc().set(submission);
     res.render('success');
@@ -73,7 +75,7 @@ app.post('/addPost', function(req, res){
 
 
 
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT || 3000, () => {
     console.log('server is live!')
 })
 
