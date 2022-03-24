3f412998e02e59b89f3731da8ecd49289aa43816
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-03-24 02:37:36 -0400

    add comments

--- index.js
+++ index.js
@@ -6,25 +6,29 @@ const ejs = require('ejs');
 const underscore = require('underscore');
 const axios = require('axios').default;
 
+//Express configuration 
 app.use(bp.urlencoded({ extended: true }));
 app.use(exp.static('public'));
 app.set('view engine', 'ejs');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is live!')
+})
 
-
+//Firebase configuration 
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 const { PassThrough } = require('stream');
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
+let blogsRef = db.collection('Blogs');
 
 
-
+//Home Route 
 app.get('/', function (req, res) {
     res.render('home');
 })
 
-let blogsRef = db.collection('Blogs');
-
+//Blog Route 
 app.get('/blog', function (req, res) {
     var posts = [];
     var eachPost = [];
@@ -43,16 +47,13 @@ app.get('/blog', function (req, res) {
 
 })
 
-app.post('/blog', function(req, res){
-    console.log(req.body.title);
-    res.render('blogpost');
-})
-
+//Blog Post Route 
 app.get('/blogpost', (req, res) => {
     console.log(req.body.title);
     res.render('blogpost');
 })
 
+//Authors Route
 app.get('/authors', function (req, res) {
     var allAuthors = [];
     blogsRef.get().then((qureySnapshot) => {
@@ -67,10 +68,10 @@ app.get('/authors', function (req, res) {
 
 })
 
+//Add Post Route 
 app.get('/addpost', function (req, res) {
     res.render('addpost');
 })
-
 app.post('/addpost', function (req, res) {
     const submission = {
         title: req.body.title,
@@ -82,10 +83,5 @@ app.post('/addpost', function (req, res) {
     res.render('success');
 })
 
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('server is live!')
-})
 
 
